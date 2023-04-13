f4b2d2467628cd6d98e1a86a19e858f078a35237
AbdullahGbale <bigimarniac@gmail.com>
2023-04-13 16:54:44 -0700

    commit msg

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @b: number of bytes top allocate
+ * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
@@ -18,3 +18,4 @@ void *malloc_checked(unsigned int b)
 
 	return (ptr);
 }
+#endif
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,8 +1,8 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * *string_nconcat - concatenates n bytes of a string to ano
- * ther string
+ * *string_nconcat - concatenates n bytes of a string to another string
  * @s1: string to append to
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
@@ -23,8 +23,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
 	if (!s)
 		return (NULL);
+
 	while (i < len1)
 	{
 		s[i] = s1[i];
@@ -35,6 +37,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s[i++] = s2[j++];
 	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
-	s[i] = '\ 0';
+
+	s[i] = '\0';
+
 	return (s);
 }
