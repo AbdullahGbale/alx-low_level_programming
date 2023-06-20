f3924fef85db118ca473d2bef3767fe5d7dd725a
AbdullahGbale <bigimarniac@gmail.com>
2023-06-20 13:45:14 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,7 +5,8 @@
  * init _dog - initializes a variable of type struct dog
  * @d:n pointer to struct dog to initialize
  * @name: name to initialize
- * @age: age to initiakllize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
