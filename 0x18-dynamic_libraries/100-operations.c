0c48fb63fd9378d48d008d6b4ea4e69088a19776
AbdullahGbale <bigimarniac@gmail.com>
2023-08-25 15:53:53 -0700

    shared

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/**
+ *  add - functions that adds numbers
+ *  @a: first number
+ *  @b: second number
+ *  Return: returns the sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - functions that subtracts numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns the subtraction value of two numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - functions that multiplies numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns the mulitplication value of two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - functions that divides numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns the quotient of dividend and divisor
+ */
+int div(int a, int b)
+{
+	if (b != 0)
+		return (a / b);
+	else
+		return (0);
+}
+
+/**
+ * mod - functions that finds the remainder
+ * @a: first number
+ * @b: second number
+ * Return: returns the remainder of dividend and divisor
+ */
+int mod(int a, int b)
+{
+	if (b != 0)
+		return (a % b);
+	else
+		return (0);
+}
